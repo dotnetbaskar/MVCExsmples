6eea6e18828c213d90085ba8de10f98338e55dd1
Baskar <baskar.info@yahoo.com>
2020-02-21 19:35:28 +0530

    Intial Commit

---
+++ MVCTraining/MVCTraining/Controllers/HTTPActionController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public class HTTPActionController : Controller
+    {
+        // GET: HTTPAction
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost,ActionName("Index")]
+        public ActionResult PostAction(string name)
+        {
+            ViewBag.NameResult = "YOu Clicked Post Action" + name;
+            return View("Index");
+        }
+
+
+        [HttpPut, ActionName("Index")]
+        public ActionResult PutAction(string name)
+        {
+            
+            ViewBag.NameResult = "YOu Clicked Put Action " + name;
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteAction()
+        {
+            return View("Index");
+        }
+
+        [HttpHead]
+        public ActionResult HeadAction()
+        {
+            return View("Index");
+        }
+
+        [HttpOptions]
+        public ActionResult OptionsAction()
+        {
+            return View("Index");
+        }
+
+        [HttpPatch]
+        public ActionResult PatchAction()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ MVCTraining/MVCTraining/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTraining.Models;
+
+namespace MVCTraining.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        // GET: Student
+        public ActionResult Find()
+        {
+            List<Student> stdList = new List<Student>() {
+                    new Student() { StudentId = 10, StudentName = "BAC", Age = 20 },
+                        new Student() { StudentId = 11, StudentName = "XYC", Age = 21 },
+                    };
+            
+              
+            
+            return View(stdList);
+        }
+        //[HttpPost]
+        //public ActionResult Find(Student std)
+        //{
+        //    ViewData["Name"] = "Welcome to" + std.StudentName;
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Find(FormCollection values)
+        //{
+        //    ViewData["Name"] = "Welcome to" + values["StudentName"];
+        //    return View();
+        //}
+        //[HttpPost]
+        public ActionResult Edit([Bind(Include = "StudentId, StudentName")] Student std)
+        {
+            var name = std.StudentName;
+
+            //write code to update student 
+
+            return RedirectToAction("Index");
+        }
+    }
+}
